bb13da6231abf9b08a09a0d27c3f460e2cd57612
YoussefEchelhi <114957292+YoussefEchelhi@users.noreply.github.com>
2023-02-09 05:32:36 +0100

    Added grayspace to menu and cookies

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const nav = document.querySelector('.nav');
 const logoBtn = document.querySelector('.logoButton');
+const navWrappper = document.querySelector('.navWrapper')
 
 logoBtn.addEventListener('click', openMenu);
 
 function openMenu() {
-    nav.classList.toggle('open');
+    navWrappper.classList.toggle('open');
     logoBtn.classList.toggle('rotate');
 }
 
 const cookies = document.querySelector('.cookies');
 const acceptBtn = document.querySelector('.acceptBtn');
 const rejectBtn = document.querySelector('.rejectBtn');
+const container5 = document.querySelector('.container5');
 
 acceptBtn.addEventListener('click', closeCookies);
 rejectBtn.addEventListener('click', closeCookies);
@@ -18,4 +20,5 @@ rejectBtn.addEventListener('click', closeCookies);
 
 function closeCookies() {
     cookies.classList.toggle('none');
+    container5.classList.toggle('none');
 }
