f4a67a4ef96caca1c3698f5321b9c397c80f6d4b
YoussefEchelhi <114957292+YoussefEchelhi@users.noreply.github.com>
2023-02-10 18:18:42 +0100

    Update

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 const nav = document.querySelector('.nav');
 const logoBtn = document.querySelector('.logoButton');
 const navWrappper = document.querySelector('.navWrapper')
+const cookies = document.querySelector('.cookies');
+const acceptBtn = document.querySelector('.acceptBtn');
+const rejectBtn = document.querySelector('.rejectBtn');
+const container4 = document.querySelector('.container4');
 
 logoBtn.addEventListener('click', openMenu);
+acceptBtn.addEventListener('click', closeCookies);
+rejectBtn.addEventListener('click', closeCookies);
 
 function openMenu() {
     navWrappper.classList.toggle('open');
     logoBtn.classList.toggle('rotate');
 }
 
-const cookies = document.querySelector('.cookies');
-const acceptBtn = document.querySelector('.acceptBtn');
-const rejectBtn = document.querySelector('.rejectBtn');
-const container5 = document.querySelector('.container5');
-
-acceptBtn.addEventListener('click', closeCookies);
-rejectBtn.addEventListener('click', closeCookies);
-
-
 function closeCookies() {
     cookies.classList.toggle('none');
-    container5.classList.toggle('none');
+    container4.classList.toggle('none');
 }
